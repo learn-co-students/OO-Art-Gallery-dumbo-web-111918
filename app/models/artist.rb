095dda028806eec686e762de2219828ebc86353b
github username <github email address>
2018-12-05 17:30:28 -0500

    adds group work

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,9 +2,39 @@ class Artist
 
   attr_reader :name, :years_experience
 
+  @@all = []
+
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+
+    @@all << self
   end
 
+  def paintings
+    Painting.all.select do |painting|
+      painting.artist == self
+    end
+  end
+
+  def galleries
+    paintings.map { |painting| painting.gallery }
+  end
+
+  def cities
+    # paintings.map do |painting|
+    #   painting.gallery.city
+    # end
+
+    galleries.map { |gallery| gallery.city }
+
+  end
+
+  def create_painting(gallery, title, price)
+    Painting.new(title, price, gallery, self)
+  end
+
+  def self.all
+    @@all
+  end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,6 +1,29 @@
 require_relative '../config/environment.rb'
 
 
+# Artist
+art1 = Artist.new('Kevin',3)
+art2 = Artist.new('Jarrian',10)
+art3 = Artist.new('Eric',5)
+art4 = Artist.new('Julian',1)
+art5 = Artist.new('Michelle',0)
+# gallery
+# (name, city)
+gal1 = Gallery.new('Dumbo Gallery','Brooklyn')
+gal2 = Gallery.new('Bronx Gallery','Bronx')
+gal3 = Gallery.new('Harlem Gallery', 'Harlem')
+gal4 = Gallery.new('Queens Gallery','Queens')
+gal5 = Gallery.new('Mex Gallery','San Fran')
+gal6 = Gallery.new('Dumbo Gallery','Brooklyn')
+# paintings
+# title, price, gallery, artist)
+paint1 = Painting.new('Mona Lisa', 10000, gal1, art5)
+paint2 = Painting.new('French Girl', 5, gal2, art4)
+paint3 = Painting.new('Night', 920, gal3, art1)
+paint4 = Painting.new('Cabin Fever', 7500, gal4, art2)
+paint5 = Painting.new('Family Jules', 500, gal5, art3)
+paint6 = Painting.new('Night 2', 400, gal3, art1)
+
 
 
 binding.pry
